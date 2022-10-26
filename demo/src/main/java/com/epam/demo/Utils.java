adc5fd826629881075d6214409bcfcd8931491e3
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-10-27 02:12:55 +0400

    gradle have a problem

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+//        return StringUtils.isPositiveNumber();
+        return  true;
     }
 }
