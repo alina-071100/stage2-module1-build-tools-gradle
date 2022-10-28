b32062b25a4fbef6c80e3f8ce0d70295cce02a34
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-10-28 13:23:39 +0400

    Build Tools. Gradle

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.toDouble(str) > 0;
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
 
     }
     }
